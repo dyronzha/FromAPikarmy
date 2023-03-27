ca449eceb9f3823f6c7eae06989fbd199a1c6f9f
dyronzha <dyron85511@gmail.com>
2023-03-28 00:04:25 +0800

    Title scene

---
+++ Assets/Scripts/GameTitleManager.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace FromAPikarmy
+{
+	public class GameTitleManager : MonoBehaviour
+	{
+
+		[SerializeField] private Button _startButton;
+		[SerializeField] private Button _instructButton;
+		[SerializeField] private Button _creditButton;
+
+		private void Awake()
+		{
+			
+		}
+	}
+}
